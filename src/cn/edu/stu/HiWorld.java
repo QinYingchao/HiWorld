333eed2201e4adc0341d616b02d85b18dd5a4b66
QinYingchao <1063492964@qq.com>
2019-09-16 13:45:02 +0800

    test change

--- src/cn/edu/stu/HiWorld.java
+++ src/cn/edu/stu/HiWorld.java
@@ -1,12 +1,13 @@
 package cn.edu.stu;
 
 public class HiWorld {
-    public static void main(String args[]){
+    public static void main(String []args){
         int count=0;
         System.out.println("Hi World!!!");
         for(int i=0;i<3;i++){
             count=count+i;
         }
         System.out.println("count="+count);
+        System.out.println("快捷键方式：CTR+K");
     }
 }
